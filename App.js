dbc9c01a6e2802b904ddb8184177048be78ea4bc
David Werner <davidwerner2@gmx.de>
2022-02-17 09:36:01 +0200

    Added Header, Footer and Style

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View } from 'react-native';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Gameboard from './components/Gameboard';
+import styles from './style/style';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header></Header>
+      <Footer></Footer>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
